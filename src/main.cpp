115b36d7349b8cb9993a9afaa86facc1daf233fb
BagelBrook <carter.brooklin@student.greenriver.edu>
2026-09-24 21:53:13 +0000

    background and speed changes

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(15, 0, 0));
+    bn::backdrop::set_color(bn::color(20, 0, 10));
 
     auto dot = bn::sprite_items::bun.create_sprite(0, 0);
 
-    bn::fixed speed = 1.5;
+    bn::fixed speed = 2.5;
 
     bn::fixed dy = 0;
     bn::fixed gravity = .03;
